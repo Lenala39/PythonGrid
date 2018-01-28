0ce3424714eaf8a68bb1d524456a710c84b14990
Jannik Schmitt <32269278+Broconuts@users.noreply.github.com>
2018-01-28 22:32:31 +0100

    split up Gridworld into the main class and two modules
    Documentation added, code cleaned.
    Note: manual mode kinda works given the correct input (for some reason incrementing the iteration steps by 1, starting with 1) but sometimes returns weird values otherwise.

--- Calculate.py
+++ Calculate.py
@@ -1,1 +1,112 @@
+def policyEvaluation(Gridworld):
+    '''
+    calculates the updated value function:
+     - iterates over grid
+     - for each field (state) calculates the new value
+     - by adding the reward to the discounted sum of possible next states
+    '''
+    # fill array with non-values for all fields we need to start
+    copiedValues = [[None for x in range(len(Gridworld.grid[0]))] for y in range(len(Gridworld.grid))]
 
+    # iterate over grid
+    for i in range(len(Gridworld.grid)):
+        for j in range(len(Gridworld.grid[0])):
+            # only if state is Free (not obstacle ...)
+            if (Gridworld.grid[i][j] == "F"):
+                # use function to get weighted sum
+                sum = possibleStates(Gridworld, Gridworld.policy[i][j], i, j)
+                # update value function with reward and GAMMA
+                copiedValues[i][j] = Gridworld.REWARD + Gridworld.GAMMA * sum
+            else:
+                copiedValues[i][j] = Gridworld.values[i][j]
+    Gridworld.values = copiedValues
+
+
+def possibleStates(Gridworld, policyValue, i, j):
+    '''
+    iterates over possible states that can be reached from state
+    :param policyValue: what action should I take?
+    :param i: index of grid
+    :param j: index of grid
+    :return: sum of all probabilities * expected reward for ending up in a certain state
+    '''
+    # calculate the action that can be done with small chance
+    if (policyValue == "up"):
+        clockwise_action = "right"
+        counterclockwise_action = "left"
+    elif (policyValue == "down"):
+        clockwise_action = "left"
+        counterclockwise_action = "right"
+    elif (policyValue == "left"):
+        clockwise_action = "up"
+        counterclockwise_action = "down"
+    elif (policyValue == "right"):
+        clockwise_action = "down"
+        counterclockwise_action = "up"
+
+    # get the indices of the next state, if ...
+    # ... action is performed correctly
+    wanted_i, wanted_j = nextState(Gridworld, policyValue, i, j)
+
+    # ... clockwise action is performed
+    clockwise_i, clockwise_j = nextState(Gridworld, clockwise_action, i, j)
+    # ... counterclockwise action is performed
+    counterclockwise_i, counterclockwise_j = nextState(Gridworld, counterclockwise_action, i, j)
+    # calculate the sum of the values with the probability to reach it
+    sum = Gridworld.values[wanted_i][wanted_j] * 0.8
+    sum = sum + Gridworld.values[clockwise_i][clockwise_j] * 0.1
+    sum = sum + Gridworld.values[counterclockwise_i][counterclockwise_j] * 0.1
+
+    # return the weighted sum of possible states and thei respective values
+    return sum
+
+
+def nextState(Gridworld, policyValue, i, j):
+    '''
+    Returns next state, taking into consideration invalid states.
+    Invalid states are fields in which obstacles are as well as fields beyond
+    the theoretical borders of our grid.
+    :param policyValue: current policy value
+    :param i: index i of grid
+    :param j: index j of grid
+    :return: new indices i and j after performing policy
+    '''
+    if (policyValue == "up" and i != 0):
+        if (Gridworld.grid[i - 1][j] != "O"):
+            i = i - 1
+    elif (policyValue == "down" and i != len(Gridworld.grid) - 1):
+        if (Gridworld.grid[i + 1][j] != "O"):
+            i = i + 1
+    elif (policyValue == "left" and j != 0):
+        if (Gridworld.grid[i][j - 1] != "O"):
+            j = j - 1
+    elif (policyValue == "right" and j != len(Gridworld.grid[0]) - 1):
+        if (Gridworld.grid[i][j + 1] != "O"):
+            j = j + 1
+
+    return i, j
+
+
+def makePolicy(Gridworld):
+    '''
+    Updates the policy according to the greedy action.
+    '''
+    # go through the whole policy to update it
+    for row in range(len(Gridworld.policy)):
+        for col in range(len(Gridworld.policy[0])):
+            # only get policy for fields which aren't an obstacle
+            if(Gridworld.policy[row][col] in Gridworld.actions):
+                temp = [None, None, None, None]
+
+                for i in range(len(temp)):
+                    temp[i] = possibleStates(Gridworld, Gridworld.actions[i], row, col)
+
+                max = temp[0]
+                ind = 0
+
+                for i in range(len(temp)):
+                    if max < temp[i]:
+                        max = temp[i]
+                        ind = i
+
+                Gridworld.policy[row][col] = Gridworld.actions[ind]
--- Gridworld.py
+++ Gridworld.py
@@ -4,7 +4,8 @@ import random
 import math
 from copy import deepcopy
 from array import array
-import Calculations
+import calculate
+import visualize
 
 
 class Gridworld:
@@ -17,17 +18,16 @@ class Gridworld:
 
     def __init__(self):
         self.actions = ["up", "down", "left", "right"]
-        self.arrows = {"up": "\u2B06", "down": "\u2B07", "left": "\u2B05", "right": "\u2B95"}
-        self.gamma = 1
         self.processingMode = "m"
         self.grid = ""
         self.policy = []
-        self.valueFunction = []
+        self.values = []
+        self.GAMMA = 1
         self.REWARD = -0.4
         self.PITFALL = -1
         self.GOAL = 1
-        self.iterations = 1
-        self.neighbourind = [(-1, 0), (1, 0), (0, -1), (0, 1)]
+        self.ITERATIONS = 1
+
 
     def read(self):
         '''
@@ -72,7 +72,7 @@ class Gridworld:
 
     def readUserInput(self):
         '''
-        Reads in processing mode and gamma value
+        Reads in processing mode and GAMMA value
         '''
 
         # read in processing mode from user (a or m)
@@ -83,46 +83,42 @@ class Gridworld:
             if ("exit" in processingMode.lower()):
                 sys.exit()
             processingMode = input("Please enter either \"a\" or \"m\": ")
-
+        self.processingMode = processingMode
 
         # if automated processing mode is choosen, get number of evaluation steps (n)
-        iterations = 1
-        if (processingMode == "a"):
-            iterations = input("Please specify a number of iterations for each evaluation phase: ")
-            if ("exit" in iterations.lower()):
+        if (processingMode == "a" or processingMode == "A"):
+            ITERATIONS = input("Please specify a number of iterations for each evaluation phase: ")
+            if ("exit" in ITERATIONS.lower()):
                 sys.exit()
             try:
-                iterations = int(iterations)
-                while (iterations <= 0):  # has to be bigger than zero
+                ITERATIONS = int(ITERATIONS)
+                while (ITERATIONS <= 0):  # has to be bigger than zero
                     # checking if user wants to exit
-                    if ("exit" in iterations.lower()):
+                    if ("exit" in ITERATIONS.lower()):
                         sys.exit()
-                    iterations = input("Please specify a positive integer for number of iterations for each evaluation phase: ")
+                    ITERATIONS = input("Please specify a positive integer for number of iterations for each evaluation phase: ")
             except ValueError:
                 print("Stop this tomfoolery and enter a positive integer! Try again next time.")
                 sys.exit()
+            self.ITERATIONS = ITERATIONS
 
-        # read in gamma as float
-        gamma = input("Please specify a gamma value between 0 and 1: ")
+        # read in GAMMA as float
+        GAMMA = input("Please specify a gamma value between 0 and 1: ")
         # check initial input for wish to end program
-        if ("exit" in gamma.lower()):
+        if ("exit" in GAMMA.lower()):
             sys.exit()
         try:
             # make sure input has acceptable value
-            while (float(gamma) > 1.0 or float(gamma) < 0.0):
-                gamma = input("Please specify a gamma value between 0 and 1: ")
-                if ("exit" in gamma.lower()):
+            while (float(GAMMA) > 1.0 or float(GAMMA) < 0.0):
+                GAMMA = input("Please specify a gamma value between 0 and 1: ")
+                if ("exit" in GAMMA.lower()):
                     sys.exit()
-            gamma = float(gamma)
+            GAMMA = float(GAMMA)
         except ValueError:
             print("Stop this tomfool ery and enter a float value between 0 and 1! Try again next time.")
             sys.exit()
+        self.GAMMA = GAMMA
 
-        self.processingMode = processingMode
-        self.iterations = iterations
-        self.gamma = gamma
-
-        # @TODO maybe implement goal > pitfall?
         # check for correctness of input
         # since it is absolutely up to the user which values these variables take
         # we only check for input type
@@ -143,194 +139,63 @@ class Gridworld:
             print("Stop this tomfool ery and enter a float value between 0 and 1! Try again next time.")
             sys.exit()
 
+
     def randomPolicyInit(self):
         '''
         Initializes random policy by assigning random value from self.actions to every state (F)
         '''
-        # print(self.actions)
-        i = 0
-        j = 0
-
+        # write None-values into all the fields we need in the new array
         self.policy = [[None for x in range(len(self.grid[0]))] for y in range(len(self.grid))]
 
+        # iterate over array (which has the same form of the grid)
         for i in range(len(self.grid)):
-            # print("row", row)
             for j in range(len(self.grid[0])):
-                # print("elem", elem)
+                # write random initial policy into all fields that are free
                 if (self.grid[i][j] == "F"):
                     self.policy[i][j] = random.choice(self.actions)
+                # mark specia fields as such
                 elif (self.grid[i][j] == "E"):
                     self.policy[i][j] = "+"
                 elif (self.grid[i][j] == "P"):
                     self.policy[i][j] = "-"
                 else:
                     self.policy[i][j] = "X"
-        # print("policy: ", self.policy)
 
-    def valueFunctionInit(self):
+
+    def valuesInit(self):
         '''
         Initializes value function
         Empty fields are given a value of 0, pitfalls are given the punishment-value determined by the user
         and a goal state is given the reward-value determined by the user
         '''
-        i = 0
-        j = 0
+        # write None-values into all the fields we need in the new array
+        self.values = [[0 for x in range(len(self.grid[0]))] for y in range(len(self.grid))]
 
-        self.valueFunction = [[0 for x in range(len(self.grid[0]))] for y in range(len(self.grid))]
-
+        # iterate over array (which has the same form of the grid)
         for i in range(len(self.grid)):
-            # print("row", row)
             for j in range(len(self.grid[0])):
-                # print("elem", elem)
+                # mark obstacle fields as non-value fields
                 if (self.grid[i][j] == "O"):
-                    self.valueFunction[i][j] = None
-
-        # print("valueFunction", self.valueFunction)
-
-    def policyEvaluation(self):
-
-        '''
-        calculates the updated value function:
-         - iterates over grid
-         - for each field (state) calculates the new value
-         - by adding the reward to the discounted sum of possible next states
-        '''
-
-        i = 0
-        j = 0
-        copiedValueFunction = [[None for x in range(len(self.grid[0]))] for y in range(len(self.grid))]
-
-        # iterate over grid
-        for i in range(len(self.grid)):
-            for j in range(len(self.grid[0])):
-                # only if state is Free (not obstacle ...)
-                if (self.grid[i][j] == "F"):
-                    # use function to get weighted sum
-                    sum = self.possibleStates(self.policy[i][j], i, j)
-                    # print("sum in policy iteration", sum)
-
-                    # update value function with reward and gamma
-                    copiedValueFunction[i][j] = self.REWARD + self.gamma * sum
-
-                elif (self.grid[i][j] == "O"):
-                    copiedValueFunction[i][j] = None
-                elif (self.grid[i][j] == "E"):
-                    copiedValueFunction[i][j] = self.GOAL
-                elif (self.grid[i][j] == "P"):
-                    copiedValueFunction[i][j] = self.PITFALL
-
-        self.valueFunction = copiedValueFunction
-        # print("new value function", self.valueFunction)
-
-    def possibleStates(self, policyValue, i, j):
-        '''
-        iterates over possible states that can be reached from state
-        :param state: current state
-        :param policyValue: what action should I take?
-        :param i: index of grid
-        :param j: index of grid
-        :return: sum of all probabilities * expected reward for ending up in a certain state
-        '''
-
-        # calculate the action that can be done with small chance
-        clockwise_action = ""
-        counterclockwise_action = ""
-
-        if (policyValue == "up"):
-            clockwise_action = "right"
-            counterclockwise_action = "left"
-        elif (policyValue == "down"):
-            clockwise_action = "left"
-            counterclockwise_action = "right"
-        elif (policyValue == "left"):
-            clockwise_action = "up"
-            counterclockwise_action = "down"
-        elif (policyValue == "right"):
-            clockwise_action = "down"
-            counterclockwise_action = "up"
-
-        # get the indices of the next state, if ...
-        # ... action is performed correctly
-        wanted_i, wanted_j = self.nextState(policyValue, i, j)
-        # print("wanted i", wanted_i, "wanted j", wanted_j)
-
-        # ... clockwise action is performed
-        clockwise_i, clockwise_j = self.nextState(clockwise_action, i, j)
-        # ... counterclockwise action is performed
-        counterclockwise_i, counterclockwise_j = self.nextState(counterclockwise_action, i, j)
-        sum = 0
-        # calculate the sum of the values with the probability to reach it
-        # print("self.valueFunction wanted", self.valueFunction[wanted_i][wanted_j])
-        sum = self.valueFunction[wanted_i][wanted_j] * 0.8
-        sum = sum + self.valueFunction[clockwise_i][clockwise_j] * 0.1
-        sum = sum + self.valueFunction[counterclockwise_i][counterclockwise_j] * 0.1
-        # print("sum", sum)
-
-        # return the weighted sum of possible states and thei respective values
-        return sum
-
-    def nextState(self, policyValue, i, j):
-        '''
-        :param state: current state on the grid
-        :param policyValue: current policy value
-        :param i: index i of grid
-        :param j: index j of grid
-        :return: new indices i and j after performing policy
-        '''
-        if (policyValue == "up" and i != 0):
-            if (self.grid[i - 1][j] != "O"):
-                i = i - 1
-        elif (policyValue == "down" and i != len(self.grid) - 1):
-            if (self.grid[i + 1][j] != "O"):
-                i = i + 1
-        elif (policyValue == "left" and j != 0):
-            if (self.grid[i][j - 1] != "O"):
-                j = j - 1
-        elif (policyValue == "right" and j != len(self.grid[0]) - 1):
-            if (self.grid[i][j + 1] != "O"):
-                j = j + 1
-
-        return i, j
-
-    def makePolicy(self):
-        # go through the whole policy to update it
-        for row in range(len(self.policy)):
-            for col in range(len(self.policy[0])):
-                # only get policy for fields which aren't a obstacle
-                if(self.policy[row][col] != '+'):
-                    if(self.policy[row][col] != '-'):
-                        if(self.policy[row][col] != 'X'):
-
-                            temp = [None, None, None, None]
-
-                            for i in range(len(temp)):
-                                temp[i] = self.possibleStates(self.actions[i], row, col)
-
-                            max = temp[0]
-                            ind = 0
-
-                            for i in range(len(temp)):
-                                if max < temp[i]:
-                                    max = temp[i]
-                                    ind = i
-
-                            self.policy[row][col] = self.actions[ind]
-        self.printPolicy()
-
+                    self.values[i][j] = None
+                # give pitfall field its value
+                if (self.grid[i][j] == "P"):
+                    self.values[i][j] = self.PITFALL
+                # give exit field its value
+                if (self.grid[i][j] == "E"):
+                    self.values[i][j] = self.GOAL
 
 
     def runPolicyIteration(self):
         '''
         runs policy iteration in one of two modes
         a: automatic mode
-            - runs evaluation phase for self.iterations-times
+            - runs evaluation phase for self.ITERATIONS-times
             - stops when there is no change in policy anymore
         m: manual mode
-            - runs evaluation phase for a designated amount of iterations
-            - user input for number of iterations for eval after each iteration
+            - runs evaluation phase for a designated amount of ITERATIONS
+            - user input for number of ITERATIONS for eval after each iteration
             - stops when there is no change in policy anymore
         '''
-
         # automatic mode
         if (self.processingMode == "a"):
 
@@ -338,50 +203,53 @@ class Gridworld:
             eq = False
             while not eq:
                 oldPolicy = deepcopy(self.policy)  # copy current policy
-                self.runEvaluation(self.iterations)  # run evaluation
-                self.printValueFunction()
-
-                self.makePolicy()  # run iteration
+                self.runEvaluation(self.ITERATIONS)  # run evaluation
+                calculate.makePolicy(self)  # run iteration
                 eq = self.comparePolicies(self.policy, oldPolicy)  # reassign equality "measure"
+            visualize.printValues(self)
+            visualize.printPolicy(self)
 
         # manual mode
         elif (self.processingMode == "m"):
 
             # compare two policies
-            eq = False
             # as long as the new one differs from the old policy
+            # ask user input for ITERATIONS again
+            eq = False
             while not eq:
-                # ask user input for iterations again
                 try:
-                    iterations = input("How many iterations should policy evaluation make? ")
-                    if ("exit" in iterations.lower()):
+                    ITERATIONS = input("How many iterations should policy evaluation make? ")
+                    if ("exit" in ITERATIONS.lower()):
                         sys.exit()
-                    iterations = int(iterations)
-                    while (iterations <= 0):
-                        iterations = input("Please enter a positive integer! Number of iterations: ")
-                        if ("exit" in iterations.lower()):
+                    ITERATIONS = int(ITERATIONS)
+                    while (ITERATIONS <= 0):
+                        ITERATIONS = input("Please enter a positive integer! Number of iterations: ")
+                        if ("exit" in ITERATIONS.lower()):
                             sys.exit()
-                        iterations = int(iterations)
+                        ITERATIONS = int(ITERATIONS)
+                    self.ITERATIONS = ITERATIONS
 
                 except ValueError:
                     print("Stop this tomfoolery and enter a positive integer! Try again next time.")
 
                 oldPolicy = deepcopy(self.policy)  # copy policy again
-                self.runEvaluation(iterations)  # run evaluation
-                self.printValueFunction()
-                self.makePolicy()  # run iteration
+                self.runEvaluation(self.ITERATIONS)  # run evaluation
+                calculate.makePolicy(self)  # run iteration
                 eq = self.comparePolicies(self.policy, oldPolicy)  # update equality-"measure"
-                # @Todo: print policy and value function
 
-    def runEvaluation(self, iterations):
+                # print the resulting values and policies
+                visualize.printValues(self)
+                visualize.printPolicy(self)
+
+
+    def runEvaluation(self, ITERATIONS):
         '''
-        runs policy evaluation for a designated amount of iterations
-        :param iterations: how often should evaluation be repeated
+        runs policy evaluation for a designated amount of ITERATIONS
+        :param ITERATIONS: how often should evaluation be repeated
         '''
-        i = 0
-        while (i <= iterations):
-            self.policyEvaluation()
-            i = i + 1
+        for i in range(ITERATIONS):
+            calculate.policyEvaluation(self)
+
 
     def comparePolicies(self, policyOne, policyTwo):
         '''
@@ -390,76 +258,19 @@ class Gridworld:
         :param policyTwo:
         :return: boolean equal
         '''
-        equal = True
-        i = 0
-        j = 0
         for i in range(len(policyOne)):
             for j in range(len(policyOne[0])):
                 if policyOne[i][j] != policyTwo[i][j]:
-                    equal = False
-        return equal
-
-    def printPolicy(self):
-        '''
-        Prints policy for each field in the grid correctly formatted in rows and columns
-        '''
-
-        for i in range(len(self.policy)):
-            for j in range(len(self.policy[0])):
-                # prints a symbol representing the direction that the policy for a given field in the grid
-                # is indicating
-                if (self.policy[i][j] == "up"):
-                    print("^", end=" ")
-                elif (self.policy[i][j] == "down"):
-                    print("v", end=" ")
-                elif (self.policy[i][j] == "left"):
-                    print("<", end=" ")
-                elif (self.policy[i][j] == "right"):
-                    print(">", end=" ")
-                else:
-                    print(self.policy[i][j], end=" ")
-            print("\n", end="")
-        print("\n", end="")
-
-    def printValueFunction(self):
-        '''
-        Prints value for each field in the grid correctly formatted in rows and columns
-        '''
-        # iterate over value function
-        for i in range(len(self.valueFunction)):
-            for j in range(len(self.valueFunction[0])):
-                # checking if nothing (or a placeholder) was written in this position of the array
-                if (self.valueFunction[i][j] == "None" or self.valueFunction[i][j] is None):
-                    print("  x  ", end=" ")
-                # printing the value of current position in the array
-                # use of ceiling function for rounding up to three decimals is used instead of
-                # Python's "round" function since it delivered better results in this program
-                else:
-                    print("{0:5}".format(math.ceil(self.valueFunction[i][j]*1000)/1000), end=" ")
-            print("\n", end="")
-        print("\n", end="")
-
-    def printGrid(self):
-        '''
-        Prints grid correctly formatted in rows and columns
-        :param grid: grid to be printed
-        '''
-
-        # for each row aka sublist
-        for row in self.grid:
-            # select every element in that sublist (row) and print it
-            for elem in row:
-                print(elem, end="")  # end="" to substitute the default end of print which is \n by nothing
-
-            print("\n", end="")  # line break after each row
+                    return False
+        return True
 
 
 if __name__ == '__main__':
     test = Gridworld()
     print("Welcome to Gridworld!\nYou may terminate the program by typing \'exit\' anytime you are asked for input.")
     test.read()
-    test.printGrid()
+    visualize.printGrid(test)
     test.readUserInput()
     test.randomPolicyInit()
-    test.valueFunctionInit()
+    test.valuesInit()
     test.runPolicyIteration()
